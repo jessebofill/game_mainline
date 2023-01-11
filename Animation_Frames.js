76cc3523d9804292e051cc59efeed75a1f93f36a
jessebofill <jesse_bofill@yahoo.com>
2023-01-11 11:22:05 -0700

    load keyframes

--- Animation_Frames.js
+++ Animation_Frames.js
@@ -1,14 +1,31 @@
-let frames = {   
-  player1:{
-    atk: {x: [[-10, 20, 'linear'], [10, 40, 'linear']]},
-  },
+let frames = {
+    player1: {
+        atk: { x: [[-10, 20, 'linear'], [10, 40, 'linear']] },
+    },
 
-  player2: {
-    atk: {x: [[10, 20, 'linear'], [-10, 40, 'linear']]}
-  
-  },
-  common: {
-    hp: {hp: [100, 50, 'linear']},
-    armor: {ap: [100, 90, 'linear']}
-  }
+    player2: {
+        atk: { x: [[10, 20, 'linear'], [-10, 40, 'linear']] }
+
+    },
+    common: {
+        hp: { hp: [100, 50, 'linear'] },
+        armor: { ap: [100, 90, 'linear'] }
+    }
+}
+
+
+function parseJSON(file_path) {
+    let parsedObject
+    fetch(file_path)
+        .then((file) => {
+            console.log(file)
+            return file.json()
+        })
+        .then((jsonString) => {
+            console.log('-- > jsonString', jsonString)
+
+
+            parsedObject = jsonString
+        })
+    return parsedObject
 }
