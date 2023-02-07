f4feb14344ba76f172e395e7ebdb30f009c680f4
Jesse Bofill <jesse_bofill@yahoo.com>
2023-02-06 18:48:07 -0700

    get port from render

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
@@ -70,6 +71,6 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(3030, () => {
-    console.log('listening on *:3030');
+server.listen(port, () => {
+    console.log('listening on *: ' + port);
 });
